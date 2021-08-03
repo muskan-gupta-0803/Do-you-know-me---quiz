9374e276271c2b13d30dfae7bbbecf4f0f698f24
muskan gupta <>
2021-08-03 20:03:01 +0000

    added code about 2 questions quiz

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 var readlineSync=require('readline-sync');
-var userName=readlineSync.question('What is your name?');
-console.log('Welcome! ' + userName);
+// var userName=readlineSync.question('What is your name?');
+// console.log('Welcome! ' + userName);
+
+var score=0;
+quesOne='Is my age older than 20';
+ansOne='yes';
+
+var userAge=readlineSync.question(quesOne);
+
+if(userAge === ansOne){
+  console.log('YOU ARE RIGHT');
+  score=score+1;
+  console.log('Score=',score);
+}
+else{
+  console.log('You are wrong');
+  score=score-1;
+  console.log('Score=',score)
+}
+
+quesTwo='Is my surname Gupta';
+ansTwo='yes';
+
+var userAge=readlineSync.question(quesTwo);
+
+if(userAge === ansTwo){
+  console.log('YOU ARE RIGHT');
+  score=score+1;
+  console.log('Score=',score);
+}
+else{
+  console.log('You are wrong');
+  score=score-1;
+  console.log('Score=',score)
+}
